8dbc1e16b7dadc646eb800502eb6fd954154ecbd
joshdabosh <josh@joshdabo.sh>
2022-02-17 06:45:14 -0500

    more stuff

--- web_api.py
+++ web_api.py
@@ -1,10 +1,71 @@
 from flask import flask
 
+import time
+
+import RPi.GPIO as GPIO
+
+from hardware_constants import *
+
+from pyfingerprint.pyfingerprint import PyFingerprint
+
+def setLEDColor(r, g, b):
+    GPIO.output(LED_RED, r)
+    GPIO.output(LED_GREEN, g)
+    GPIO.output(LED_BLUE, b)
+
+def setMotorStep(w1, w2, w3, w4):
+    GPIO.output(MOTORDRIVER_INPUT1, w1)
+    GPIO.output(MOTORDRIVER_INPUT2, w2)
+    GPIO.output(MOTORDRIVER_INPUT3, w3)
+    GPIO.output(MOTORDRIVER_INPUT4, w4)
+
+
+def motorClockwise(cycleCount):
+    for i in range(cycleCount):
+        for coilsPower in MOTOR_CYCLE:
+            setMotorStep(*coilsPower)
+            time.sleep(0.001)
+
+
+def motorCounterclockwise(cycleCount):
+    for i in range(cycleCount):
+        for coilsPower in MOTOR_CYCLE[::-1]:
+            setMotorStep(*coilsPower)
+            time.sleep(0.001)
+
+
+lock = 0
 app = Flask(__name__)
 
 @app.route("/asdffdsa123")
 def open():
-    print(1)
+    if lock:
+        return "-1"
+
+    lock = 1
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(MOTORDRIVER_INPUT1, GPIO.OUT)
+    GPIO.setup(MOTORDRIVER_INPUT2, GPIO.OUT)
+    GPIO.setup(MOTORDRIVER_INPUT3, GPIO.OUT)
+    GPIO.setup(MOTORDRIVER_INPUT4, GPIO.OUT)
+
+    GPIO.setup(LED_RED, GPIO.OUT)
+    GPIO.setup(LED_GREEN, GPIO.OUT)
+    GPIO.setup(LED_BLUE, GPIO.OUT)
+    
+    motorClockwise(LATCH_CYCLE_COUNT)
+    time.sleep(3)
+
+    motorCounterclockwise(LATCH_CYCLE_COUNT-20)
+
+    setMotorStep(0, 0, 0, 0)        # don't burn out the motor waiting for fingerprints
+
+    GPIO.cleanup()
+
+
     return "1"
 
 app.run()
